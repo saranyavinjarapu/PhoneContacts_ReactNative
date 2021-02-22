3751f05bf4c4bc5b3e78c68cedf4983597138a5c
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2021-02-22 23:18:42 +0530

    imported and added OpenContactsMainScreen to App component to display open phone contacts button

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
+import OpenContactsMainScreen from "./screens/OpenContactsMainScreen";
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="PHONE CONTACTS"></Header>
+      <OpenContactsMainScreen></OpenContactsMainScreen>
     </View>
   );
 }
