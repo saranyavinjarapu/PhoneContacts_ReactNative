8520ae18fe3313275f9f805b38ab66de01dbcf7a
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2021-02-22 23:19:29 +0530

    added styling to open contacts button and the container

--- screens/OpenContactsMainScreen.js
+++ screens/OpenContactsMainScreen.js
@@ -4,8 +4,9 @@ import { StyleSheet, View, Button, Alert } from "react-native";
 const OpenContactsMainScreen = () => {
   return (
     <View style={styles.screen}>
-      <View>
+      <View style={styles.buttonContainer}>
         <Button
+          textStyle={{ color: "#FFFFFF", fontSize: 400 }}
           title="OPEN PHONE CONTACTS"
           onPress={() => console.log("open contacts button clicked")}
           style={styles.openContactButton}
@@ -19,6 +20,26 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 50,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    justifyContent: "center",
+    height: 200,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 20,
+    shadowOpacity: 0.26,
+    backgroundColor: "white",
+    elevation: 15,
+    padding: 20,
+    borderRadius: 50,
+    marginTop: 100,
+  },
+  openContactButton: {
+    borderColor: "black",
+    borderWidth: 5,
   },
 });
 
