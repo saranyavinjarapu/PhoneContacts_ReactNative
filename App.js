a47c3f6f4aad97eec6736ed73afed29770943b03
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2021-02-22 23:46:28 +0530

    Added conditional rendering of open contacts and display contacts screen and added showSelectedContactHandler to further show single contact selected data

--- App.js
+++ App.js
@@ -2,21 +2,34 @@ import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
 import OpenContactsMainScreen from "./screens/OpenContactsMainScreen";
+import DisplayContactListScreen from "./screens/DisplayContactListScreen";
 
 export default function App() {
   const [contactsData, setContactsData] = useState();
   const showContactsHandler = (contacts_data) => {
     setContactsData(contacts_data);
   };
+  const showSelectedContactHandler = (selected_contact_data) => {
+    console.log("contact selected");
+  };
+
+  let content = (
+    <OpenContactsMainScreen
+      showContacts={showContactsHandler}
+    ></OpenContactsMainScreen>
+  );
+  if (contactsData) {
+    content = (
+      <DisplayContactListScreen
+        contacts_data={contactsData}
+        showSelectedContact={showSelectedContactHandler}
+      ></DisplayContactListScreen>
+    );
+  }
   return (
     <View style={styles.screen}>
       <Header title="PHONE CONTACTS"></Header>
-      <OpenContactsMainScreen
-        showContacts={showContactsHandler}
-      ></OpenContactsMainScreen>
-      <DisplayContactListScreen
-        contacts_data={contactsData}
-      ></DisplayContactListScreen>
+      {content}
     </View>
   );
 }
