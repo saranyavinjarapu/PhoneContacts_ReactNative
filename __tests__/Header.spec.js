f6b29d9ee8b3120d34b23dea69290f2192375cd0
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2021-02-23 00:16:43 +0530

    added tests for Header comp

---
+++ __tests__/Header.spec.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import Header from "../components/Header";
+
+describe("Header Renders Correctly", () => {
+  it("renders correctly", () => {
+    const snap = render(<Header />).toJSON();
+    expect(snap).toMatchSnapshot();
+  });
+  it("Header title Works", () => {
+    const headerRender = render(<Header title="Hello Test!" />);
+    expect(headerRender).toBeTruthy();
+    const headerText = headerRender.getByTestId("header_text");
+    expect(headerText.props.children).toEqual("Hello Test!");
+  });
+});
