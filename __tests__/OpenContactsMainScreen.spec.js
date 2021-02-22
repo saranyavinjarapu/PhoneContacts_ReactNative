c3ec99a3c12e70e6033caca0ae2f81878efc79a1
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2021-02-23 00:14:36 +0530

    added tests for OpenContactsMainScreen comp

---
+++ __tests__/OpenContactsMainScreen.spec.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import OpenContactsMainScreen from "../screens/OpenContactsMainScreen";
+
+describe("Open Contacts Main Screen Renders Correctly", () => {
+  it("renders correctly", () => {
+    const snap = render(<OpenContactsMainScreen />).toJSON();
+    expect(snap).toMatchSnapshot();
+  });
+
+  it("Open Contacts Button Works", () => {
+    const { queryByText } = render(<OpenContactsMainScreen />);
+    const sendHandler = jest.fn();
+    const button = queryByText("OPEN PHONE CONTACTS");
+    expect(button).toBeTruthy();
+    fireEvent.press(button);
+    expect(sendHandler).not.toBeNull();
+  });
+});
