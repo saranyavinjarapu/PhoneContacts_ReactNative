e45b5963ed953cdf1a17140b94029fd73dfd7351
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2021-02-22 23:22:13 +0530

    added open contacts button handler that retrieves conatcts using expo

--- screens/OpenContactsMainScreen.js
+++ screens/OpenContactsMainScreen.js
@@ -1,14 +1,39 @@
 import React from "react";
 import { StyleSheet, View, Button, Alert } from "react-native";
+import * as Permissions from "expo-permissions";
+import * as Contacts from "expo-contacts";
 
-const OpenContactsMainScreen = () => {
+const OpenContactsMainScreen = (props) => {
+  const openContactsHandler = async () => {
+    const { status, permissions } = await Permissions.askAsync(
+      Permissions.CONTACTS
+    );
+    if (status === "granted") {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [
+          Contacts.Fields.Name,
+          Contacts.Fields.PhoneNumbers,
+          Contacts.Fields.Image,
+        ],
+        sort: Contacts.SortTypes.FirstName,
+      });
+
+      if (data.length > 0) {
+        props.showContacts(data);
+      } else {
+        Alert.alert("No Contacts Found");
+      }
+    } else {
+      Alert.alert("Permission to access contacts denied");
+    }
+  };
   return (
     <View style={styles.screen}>
       <View style={styles.buttonContainer}>
         <Button
           textStyle={{ color: "#FFFFFF", fontSize: 400 }}
           title="OPEN PHONE CONTACTS"
-          onPress={() => console.log("open contacts button clicked")}
+          onPress={openContactsHandler}
           style={styles.openContactButton}
         ></Button>
       </View>
